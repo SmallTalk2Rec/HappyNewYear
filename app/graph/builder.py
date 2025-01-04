fd9e46d5b3e7c071a0bd12f612393ebf807f48e4
katalria <wawa0070@naver.com>
2025-01-04 14:17:56 +0900

    feat : langraph 객체화 및 유저별 대화 가능하게 별도로 생성하는 코드 추가

--- app/graph/builder.py
+++ app/graph/builder.py
@@ -9,31 +9,46 @@ from graph.prompt import SUPERVISOR_AGENT, RECOMMEND_MOVIE_AGENT
 
 load_dotenv()
 
-llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
 
+class ConversationLangGraph:
+    def __init__(self):
+        """Initialize the ConversationLangGraph object."""
+        self.llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+        self.workflow = StateGraph(GraphState)
 
-workflow = StateGraph(GraphState)
+        # Define the nodes
+        self.workflow.add_node(
+            "supervisor_node",
+            SupervisorNode(llm=self.llm, system_template=SUPERVISOR_AGENT),
+        )
 
-# Define the nodes
-workflow.add_node(
-    "supervisor_node", SupervisorNode(llm=llm, system_template=SUPERVISOR_AGENT)
-)
+        self.workflow.add_node(
+            "recommend_movie_node",
+            RecommendMovieNode(
+                llm=self.llm,
+                tools=[
+                    MovieRetrieverTool(
+                        movie_data_path="./data/241210/movie_info_watch.csv",
+                        vectorstore_dir="./data/chroma",
+                    )
+                ],
+                system_template=RECOMMEND_MOVIE_AGENT,
+            ),
+        )
 
-workflow.add_node(
-    "recommend_movie_node",
-    RecommendMovieNode(
-        llm=llm,
-        tools=[
-            MovieRetrieverTool(
-                movie_data_path="./data/241210/movie_info_watch.csv",
-                vectorstore_dir="./data/chroma",
-            )
-        ],
-        system_template=RECOMMEND_MOVIE_AGENT,
-    ),
-)
+        # Define edges
+        self.workflow.add_edge(START, "supervisor_node")
 
-workflow.add_edge(START, "supervisor_node")
+        # Compile the workflow
+        self.graph = self.workflow.compile()
 
-# Compile the workflow
-graph = workflow.compile()
+    def get_graph(self):
+        """Return the compiled graph instance."""
+        return self.graph
+
+    def run(self, message):
+        """Run the graph with user message"""
+        grapn_response = self.graph.invoke({"messages": str(message)})["messages"][
+            -1
+        ].content
+        return grapn_response
--- app/router/chat_router.py
+++ app/router/chat_router.py
@@ -1,17 +1,27 @@
-from graph.builder import graph
-
 from fastapi import APIRouter, HTTPException
+from starlette.requests import Request
+from starlette.responses import Response
+from dataclasses import dataclass
+import datetime
+import pytz
+import os
 
 from config import access_kakao
 from app import my_app
+from graph.builder import ConversationLangGraph
 
-from pydantic import BaseModel
-from starlette.requests import Request
-from starlette.responses import Response
-import datetime, pytz
+
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_PROJECT"] = "smalltalk2rec"
 
 
 router = APIRouter()
+
+
+@dataclass
+class APIResponse:
+    version: str
+    template: dict
 
 
 @router.get("/")
@@ -39,24 +49,20 @@ async def handle_callback(request: Request):
 
         if user_id not in my_app.user_conversations:
             # 할당된 chain이 없으면 생성 후 할당
-            my_app.user_conversations[user_id] = ConversationalRetrievalChain(
-                my_app.retriever
-            )
+            my_app.user_conversations[user_id] = ConversationLangGraph().get_graph()
 
         # 사전에 할당해 놓은 chain 불러와서 사용
-        chain = my_app.user_conversations[user_id]
+        graph = my_app.user_conversations[user_id]
 
-        # chain 결과 받아오기
-        main_chain_result = chain.run(question)
+        # graph 결과 받아오기
+        bot_response = graph.run(message)
 
-        # 사용자 메시지 처리
-        bot_response = graph.invoke({"messages": str(message)})["messages"][-1].content
+        # response 형태 수정
+        response = APIResponse(
+            version="2.0",
+            template={"outputs": [{"simpleText": {"text": bot_response}}]},
+        )
+        return response
 
-        return {
-            "version": "2.0",
-            "template": {"outputs": [{"simpleText": {"text": bot_response}}]},
-        }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-        # 카카오에서 전달받은 데이터 확인
