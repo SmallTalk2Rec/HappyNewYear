7fffa1e0affb53cfb3acb2389e207f5620263761
katalria <wawa0070@naver.com>
2025-01-04 20:06:55 +0900

    fix : access token 업데이트 하는 코드 수정3

--- app/router/common_router.py
+++ app/router/common_router.py
@@ -16,7 +16,7 @@ async def kakao_login():
     """
     Kakao Authorization URL로 리다이렉트.
     """
-    authorization_url = f"https://kauth.kakao.com/oauth/authorize?client_id={KAKAO_REST_API_KEY}&redirect_uri={KAKAO_REDIRECT_URL}/oauth&response_type=code"
+    authorization_url = f"https://kauth.kakao.com/oauth/authorize?client_id={KAKAO_REST_API_KEY}&redirect_uri={KAKAO_REDIRECT_URL}&response_type=code"
     # return RedirectResponse(authorization_url)  ## 디버깅 필요
     return authorization_url
 
