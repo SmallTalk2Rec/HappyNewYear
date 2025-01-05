f9e8c762b365039c5e318842ba1448365400af79
mkk4726 <mkk4726@naver.com>
2025-01-05 15:49:52 +0900

    update files

--- data_scraping/test.py
+++ data_scraping/test.py
@@ -1,64 +1,71 @@
 from playwright.sync_api import sync_playwright
+from bs4 import BeautifulSoup
+from lxml import etree
 import time
-from assets.utils.txt import append_to_txt
 
-
-def fetch_with_scroll(page):
-    # JavaScript로 스크롤을 끝까지 내리기
-    previous_height = 0
-    i = 1
-    while True:
-        current_height = page.evaluate("document.body.scrollHeight")
-        if current_height == previous_height:
-            break
-        previous_height = current_height
-        page.evaluate("window.scrollTo(0, document.body.scrollHeight)")
-        # print(f"scroll down {i}", end='\r')
-        i += 1
-        time.sleep(2)  # 콘텐츠 로딩을 기다림
-
-# 왓챠피디아 코멘트 더보기 url
-url = f'https://pedia.watcha.com/en-KR/contents/mO88EDO/comments'
+movie_url = "https://pedia.watcha.com/ko-KR/contents/tRzL8nV"
 
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
-    page = browser.new_page()
-    page.goto(url)
+    context = browser.new_context(
+        user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    )
+    page = context.new_page()
+    page.goto(movie_url, timeout=600000)
 
-    # 스크롤 끝까지 내리기
-    fetch_with_scroll(page)
-    # print("\nScroll end")
+    try:
+        button_selector = (
+            'button.hsDVweTz[type="button"]:has-text("3일 동안 보지 않기")'
+        )
 
-    data_list = []
-    i = 95
+        button = page.locator(button_selector)
+        button.click(force=True)
+    except Exception as e:
+        print(e)
 
-    # 각 코멘트 정보 수집
-    custom_id_xpath = f'//*[@id="root"]/div[1]/section/section/div/div/div/ul/div[{i}]/div[1]/div[1]/a'
-    comment_xpath = f'//*[@id="root"]/div[1]/section/section/div/div/div/ul/div[{i}]/div[2]/a/div/div'
-    rating_xpath = f'//*[@id="root"]/div[1]/section/section/div/div/div/ul/div[{i}]/div[1]/div[2]/span'
-    n_likes_xpath = f'//*[@id="root"]/div[1]/section/section/div/div/div/ul/div[{i}]/div[3]/em[1]'
+    # 출연/제작 정보
+    cast_production_info_list = []
 
-    if page.locator(custom_id_xpath).count() > 0:
-        custom_id = page.locator(custom_id_xpath).get_attribute('href').split('/')[-1]
-    else:
-        print('hi')
+    while True:
+        content = page.content()
 
-    if page.locator(comment_xpath).count() > 0:
-        comment = page.locator(comment_xpath).inner_text().replace('\n', ' ').replace('\t', ' ').replace('\r', ' ').replace('/', '')
-    else:
-        comment = None
+        soup = BeautifulSoup(content, "html.parser")
+        tree = etree.HTML(str(soup))
 
-    if page.locator(rating_xpath).count() > 0:
-        rating = page.locator(rating_xpath).inner_text().replace('\n', ' ').replace('/', ' ')
-    else:
-        rating = None
+        i = 1
+        while True:
+            try:
+                name = tree.xpath(
+                    f'//*[@id="content_credits"]/section/div[1]/ul/li[{i}]/a/div[2]/div[1]/div[1]/text()'
+                )[0].replace("/", " ")
+                role = tree.xpath(
+                    f'//*[@id="content_credits"]/section/div[1]/ul/li[{i}]/a/div[2]/div[1]/div[2]/text()'
+                )[0].replace("/", " ")
+                unique_id = (
+                    page.locator(
+                        f'//*[@id="content_credits"]/section/div[1]/ul/li[{i}]/a'
+                    )
+                    .get_attribute("href")
+                    .split("/")[-1]
+                )
 
-    if page.locator(n_likes_xpath).count() > 0:
-        n_likes = page.locator(n_likes_xpath).inner_text().replace('\n', ' ').replace('/', ' ')
-    else:
-        n_likes = None
+                cast_production_info_list.append((name, role, unique_id))
+                i += 1
+            except Exception:
+                break
 
-    # print("\n End")
-    browser.close()  # Playwright 컨텍스트 내에서 브라우저 닫기
-    
-append_to_txt("./data/movie_comments.txt", ["test", *[custom_id, comment, rating, n_likes]])
+        button_selector = '//*[@id="content_credits"]/section/div[2]/button'
+        button = page.locator(button_selector)
+
+        if button.count() > 0:  # 버튼이 존재하는지 확인
+            try:
+                button.click(force=True)
+
+            except Exception:
+                break
+        else:
+            break
+
+print(cast_production_info_list)
+
+# browser.close()
