1c863e0c30efb3930df1015ed38f2d086ff468fe
sinjy1203 <sinjy1203@gmail.com>
2025-01-05 18:51:22 +0900

    feat: execute_tool node 추가, 줄거리 기반 semantic search 가능하도록 chroma vector store 추가

--- src/graph/node.py
+++ src/graph/node.py
@@ -3,7 +3,7 @@ from langgraph.types import Command
 from langgraph.graph import END
 from pydantic import BaseModel, Field
 
-from graph.state import GraphState
+from graph.state import GraphState, GraphConfig
 
 
 class Router(BaseModel):
@@ -14,15 +14,18 @@ class Router(BaseModel):
 class SupervisorNode:
     def __init__(self, llm, system_template):
         self.llm = llm.with_structured_output(Router)
-        self.system_message = [
-            {
-                "role": "system",
-                "content": system_template,
-            }
-        ]
+        self.system_message = {
+            "role": "system",
+            "content": system_template,
+        }
     
-    def __call__(self, state: GraphState) -> GraphState:
-        messages = self.system_message + state.messages + state.inter_messages
+    def __call__(self, state: GraphState, config: GraphConfig) -> GraphState:
+        agent_results = "\n\n".join(state.agent_results)
+        human_message = {
+            "role": "human",
+            "content": f"<user>\n{state.messages[-1].content}\n<user>\n\n{agent_results}"
+        }
+        messages = [self.system_message] + state.messages[:-1] + [human_message]
         response = self.llm.invoke(messages)
 
         if response.target == "RecommendMovieAgent":
@@ -30,9 +33,8 @@ class SupervisorNode:
                 update={
                     "inter_messages": [
                         {
-                            "role": "assistant",
-                            "name": "SupervisorAgent",
-                            "content": response.message
+                            "role": "human",
+                            "content": f"{agent_results}\n\n<user>\n{response.message}\n<user>"
                         }
                     ]
                 },
@@ -44,7 +46,6 @@ class SupervisorNode:
                     "messages": [
                         {
                             "role": "assistant",
-                            "name": "SupervisorAgent",
                             "content": response.message
                         }
                     ]
@@ -58,19 +59,71 @@ class SupervisorNode:
 
 class RecommendMovieNode:
     def __init__(self, llm, tools, system_template):
-        self.agent = create_react_agent(llm, tools=tools, state_modifier=system_template)
+        self.llm_with_tools = llm.bind_tools(tools)
+        self.system_message = {
+            "role": "system",
+            "content": system_template,
+        }
     
-    def __call__(self, state: GraphState) -> GraphState:
-        result = self.agent.invoke({"messages": state.inter_messages})
+    def __call__(self, state: GraphState, config: GraphConfig) -> GraphState:
+        messages = [self.system_message] + state.inter_messages 
+        result = self.llm_with_tools.invoke(messages)
+
+        if result.tool_calls:
+            if state.execute_tool_count >= config["configurable"]["max_execute_tool"]:
+                return Command(
+                    update={
+                        "agent_results": [
+                            f"Tool execution limit reached. Please try again later."
+                        ],
+                        "execute_tool_count": 0
+                    },
+                    goto="supervisor_node"
+                )
+            else:
+                return Command(
+                    update={
+                        "inter_messages": state.inter_messages + [result],
+                    },
+                    goto="execute_tool"
+                )
+        else:
+            return Command(
+                update={
+                    "agent_results": [
+                        f"<RecommendMovieAgent>\n{result.content}\n<RecommendMovieAgent>"
+                    ],
+                    "execute_tool_count": 0
+                },
+                goto="supervisor_node"
+            )
+
+
+class ExecuteToolNode:
+    def __init__(self, tools):
+        self.tools_by_name = {tool.name: tool for tool in tools}
+
+    def __call__(self, state: GraphState):
+        outputs = []
+
+        for tool_call in state.inter_messages[-1].tool_calls:
+            tool_result = self.tools_by_name[tool_call["name"]].invoke(
+                tool_call["args"]
+            )
+
+            outputs.append(
+                {
+                    "role": "tool",
+                    "name": tool_call["name"],
+                    "content": tool_result,
+                    "tool_call_id": tool_call["id"],
+                }
+            )
+
         return Command(
             update={
-                "inter_messages": [
-                    {
-                        "role": "assistant",
-                        "name": "RecommendMovieAgent",
-                        "content": result["messages"][-1].content
-                    }
-                ]
+                "inter_messages": state.inter_messages + outputs,
+                "execute_tool_count": state.execute_tool_count + 1,
             },
-            goto="supervisor_node"
+            goto="recommend_movie_node",
         )
--- src/test.py
+++ src/test.py
@@ -1,2 +1,12 @@
+import os
+from dotenv import load_dotenv
 from graph.builder import graph
-response = graph.invoke({"messages": [("human", "액션영화 아무거나 추천해줘")]})
+
+load_dotenv()
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_PROJECT"] = "smalltalk2rec"
+
+query = " 추천해줘"
+
+response = graph.invoke({"messages": [("human", query)]}, config={"configurable": {"max_execute_tool": 3}})
+print(response)
